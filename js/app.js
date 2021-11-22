3f93b92e364765edc3415f3468a73bad1c7d5fb2
DanielTrue <danielokezie75@gmail.com>
2021-11-22 10:32:31 +0100

    Just Finished the web design moving to using the javascript

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    function any() {
+        $(".__date__").text(new Date().getFullYear());
+    }
+    //Events
+    any();
+    //
+    for (let i = 0; i < $(".cta").length; i++) {
+        $(".cta").click(function () {
+            $("body, html")
+        });
+    }
+});
